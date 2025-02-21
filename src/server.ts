2a26fe5045acf62d7d6b395fd8c0e685fd8ff60a
Joao Piologo <joaotpiologo@hotmail.com>
2025-02-21 00:12:14 -0300

    Patch routes and swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { fastify } from "fastify";
 import { fastifyCors } from '@fastify/cors'
-import { validatorCompiler, serializerCompiler, type ZodTypeProvider, } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -12,32 +14,33 @@ app.register(fastifyCors, {
     origin: 'true', //Colocar a url do meu front quando possivel
 })
 
-app.post('/subscriptions', {
-    schema: {
-			body: z.object({
-				name: z.string(),
-				email: z.string().email(),
-			}),
-    },
-}, (request, reply) => {
-    const { name, email } = request.body
-
-		//Criação do registro no DB
-
-		return reply.status(201).send({
-			name,
-			email
-		})
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'NLW Connect',
+			version: '0.0.1',
+		},
+	},
+	transform: jsonSchemaTransform,
 })
+
+app.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP Server Runing ')
 })
 
+//install docker
 //npm i tsx typescript @types/node -D : Converte código TS em JS
 //npx tsc --init
 //npm i @fastify/cors
 //npm i zod
 //npm i fastify-type-provider-zod
+//REST Client vsc extention
+//npm i @fastify/swagger @fastify/swagger-ui
 
 //To run: npx tsx src/server.ts
